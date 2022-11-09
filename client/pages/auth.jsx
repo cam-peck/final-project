dbdd035d41c2f256aa04a500a39c9d62e8a9bb46
Cameron <cameronpeck317@gmail.com>
2022-11-09 15:39:40 +0000

    set-up auth.jsx file with AuthPage class and import Redirect, AuthForm, and AppContext #18

---
+++ client/pages/auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Redirect from '../components/redirect';
+// import AuthForm from '../components/auth-form';
+// import AppContext from '../lib/app-context';
+
+export default class AuthPage extends React.Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
