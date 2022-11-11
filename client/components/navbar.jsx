425310a072d8ff98dbacdfa730cc41332802f80a
Cameron <cameronpeck317@gmail.com>
2022-11-11 21:56:57 +0000

    add hamburger menu to navbar and change from class to function component for practicing hooks #1

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../lib';
+import { Turn as Hamburger } from 'hamburger-react';
 
-export default class Navbar extends React.Component {
+export default function Navbar(props) {
 
-  render() {
-    const { handleSignOut, user } = this.context;
-    const signInNavbar = (
-      <nav className="container-lg mx-auto py-5 bg-blue-600">
-        <div className="flex items-center justify-center">
-          {/* Logo */}
-          <div>
-            <h1 className="logo-text text-white text-2xl">RunningFuze</h1>
-          </div>
+  const { user, handleSignOut } = useContext(AppContext);
+  const [isOpen, setOpen] = useState(false);
+
+  const signInNavbar = (
+    <nav className="container-lg mx-auto py-5 bg-blue-600">
+      <div className="flex items-center justify-center">
+        {/* Logo */}
+        <div>
+          <h1 className="logo-text text-white text-2xl">RunningFuze</h1>
         </div>
-      </nav>
-    );
-    const homeNavbar = (
-      <nav className="container-lg mx-auto p-6 bg-blue-600">
-        <div className="flex items-center justify-between">
-          {/* Logo */}
-          <div>
-            <h1 className="logo-text text-white text-2xl">RunningFuze</h1>
-          </div>
-          {/* Menu Items */}
-          <div className="space-x-6 md:flex text-white">
-            <a href="#" className="hover:text-darkGrayishBlue">My Runs</a>
-            <a href="#" className="hover:text-darkGrayishBlue">My Workouts</a>
-          </div>
+      </div>
+    </nav>
+  );
+
+  const homeNavbar = (
+    <header className="w-full p-6 bg-blue-600">
+      <nav className="max-w-6xl mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div className='flex gap-3'>
+          <i className="fa-solid fa-person-running text-white text-3xl" />
+          <h1 className="logo-text text-white text-2xl">RunningFuze</h1>
+        </div>
+        {/* Menu Items */}
+        <div className="space-x-6 text-white items-center hidden md:block">
+          <a href="#" className="hover:text-darkGrayishBlue">My Runs</a>
+          <a href="#" className="hover:text-darkGrayishBlue">My Workouts</a>
           {/* Button */}
-          <a
-            onClick={handleSignOut}
-            className="hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block"
-          >Sign out
-          </a>
+          <button onClick={handleSignOut} className="text-white bg-red-500 p-3 rounded-xl">Sign out</button>
+        </div>
+        <div className="md:hidden">
+          <Hamburger toggled={isOpen} toggle={setOpen} color="white" />
         </div>
       </nav>
-    );
-    const navBar = user === null
-      ? signInNavbar
-      : homeNavbar;
-    return (
-      navBar
-    );
-  }
+    </header>
+  );
+  const navBar = user === null
+    ? signInNavbar
+    : homeNavbar;
+  return (
+    navBar
+  );
 }
-Navbar.contextType = AppContext;
