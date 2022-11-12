d888b3d67837a6df43ef21a7a2e21ac102f5b583
Cameron <cameronpeck317@gmail.com>
2022-11-12 15:55:06 +0000

    add validation component and validation functions (empty for now) to lib folder and export fomr index.js #1

--- client/components/run-form.jsx
+++ client/components/run-form.jsx
@@ -12,11 +12,14 @@ export default class RunForm extends React.Component {
       distance: 0,
       hasGpx: null
     };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    // handleChange code here
-
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
 
   handleSubmit(event) {
@@ -31,8 +34,10 @@ export default class RunForm extends React.Component {
     return (
       <div className="max-w-md mx-auto">
         <form onSubmit={this.handleSubmit}>
-          <FloatingInput type="email" name="email" placeholder="Email Address" value={title} onChange={handleChange} />
-          <FloatingInput type="password" name="password" placeholder="Password" value={title} onChange={handleChange} />
+          <FloatingInput type="date" name="date" placeholder="Email Address" value={date} onChange={handleChange} />
+          <FloatingInput type="time" name="duration" placeholder="" value={duration} onChange={handleChange} />
+          <FloatingInput type="text" name="distance" placeholder="0.00 mi" value={distance} onChange={handleChange} />
+          <FloatingInput type="string" name="pace" placeholder="0:00 / mi" onChange={handleChange} />
           <div className="pl-4 pr-4">
             <button className="w-full bg-blue-500 transition ease-in-out duration-300 hover:bg-blue-600 text-white p-3 rounded-lg font-bold text-lg">Add run</button>
           </div>
--- client/lib/index.js
+++ client/lib/index.js
@@ -1,2 +1,6 @@
 export { default as parseRoute } from './parse-route';
 export { default as AppContext } from './app-context';
+export { default as validateDescription } from './validate-description';
+export { default as validateDistance } from './validate-distance';
+export { default as validateDuration } from './validate-duration';
+export { default as validateTitle } from './validate-title';
