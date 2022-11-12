4d33cbf3a6a26233f4bab87b9be74d41ca05421f
Cameron <cameronpeck317@gmail.com>
2022-11-12 14:34:53 +0000

    move click event listener for closing drawer out of nav-items component and onto the div that holds the nav-items in drawer component #1

--- client/components/drawer-menu.jsx
+++ client/components/drawer-menu.jsx
@@ -1,34 +1,27 @@
 import React, { useContext } from 'react';
+import NavItems from './nav-items';
 import { AppContext } from '../lib';
 
 export default function DrawerMenu(props) {
   const { handleSignOut } = useContext(AppContext);
 
+  const isOpen = props.isOpen
+    ? 'left-0'
+    : '-left-56';
   return (
-    <div className="absolute top-0 left-0 bottom-0 w-48 surface-bg-color opacity shadow-xl flex flex-col justify-between rounded-br-xl transition ease-in md:hidden z-10">
+    <div className={`absolute top-0 ${isOpen} bottom-0 w-56 surface-bg-color opacity shadow-xl flex flex-col justify-between rounded-br-xl transition-left ease-in-out duration-200 md:hidden z-10`}>
+      {/* Title & Nav-items */}
       <section>
-        {/* Title & Nav-items */}
         <div className="flex justify-center items-center mt-8 mb-8">
-          {/* Title */}
-          <h1 className="logo-text text-xl on-container-color">RunningFuze</h1>
+          <h1 className="logo-text text-2xl on-container-color">RunningFuze</h1>
         </div>
-        {/* Nav-items */}
-        <div className="flex flex-col pb-8 border-b-2 border-gray-500">
-          {/* Item 1 */}
-          <div className="flex items-center hover:bg-gray-300 hover:cursor-pointer pt-4 pb-4 pl-6">
-            <i className="fa-solid fa-person-running text-2xl pr-3" />
-            <a className="text-lg" onClick={() => { props.setOpen(false); }} href="#">My Runs</a>
-          </div>
-          {/* Item 2 */}
-          <div className="flex items-center hover:bg-gray-300 hover:cursor-pointer pt-4 pb-4 pl-6">
-            <i className="fa-solid fa-dumbbell text-lg pr-3" />
-            <a href="#sign-in" onClick={() => { props.setOpen(false); }} className="text-lg">My Workouts</a>
-          </div>
+        <div onClick={() => { props.setOpen(false); }} className="flex flex-col pb-8 border-b-2 border-gray-500 m-2">
+          <NavItems />
         </div>
       </section>
       {/* Sign-out Button */}
       <section className='flex justify-end'>
-        <button onClick={() => { props.setOpen(false); handleSignOut(); }} className="text-white bg-red-500 p-3 rounded-xl flex items-center gap-2">
+        <button onClick={() => { props.setOpen(false); handleSignOut(); }} className="w-full text-white bg-red-500 p-8 rounded-br-xl flex items-center justify-center gap-2">
           <p>Sign out</p>
           <i className="fa-solid fa-right-from-bracket" />
         </button>
--- client/components/home-nav.jsx
+++ client/components/home-nav.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import DrawerMenu from './drawer-menu';
+import NavItems from './nav-items';
 import { AppContext } from '../lib';
 import { Turn as Hamburger } from 'hamburger-react';
 
@@ -7,34 +8,30 @@ export default function HomeNavbar(props) {
   const { handleSignOut } = useContext(AppContext);
   const [isOpen, setOpen] = useState(false);
 
-  const drawerMenu = isOpen
-    ? <DrawerMenu setOpen={setOpen} />
-    : '';
-
   const darkBackground = isOpen
-    ? <div className="bg-gray-900 opacity-40 absolute top-0 left-0 bottom-0 right-0 z-0" onClick={() => { setOpen(false); }} />
-    : '';
+    ? <div className="bg-gray-900 opacity-40 absolute top-0 left-0 bottom-0 right-0 z-0 transition-all ease-in-out duration-300" onClick={() => { setOpen(false); }} />
+    : <div className="bg-gray-900 opacity-0 absolute z-0" />;
 
   return (
     (
-      <header className="w-full p-6 bg-blue-600">
+      <header className="w-full pl-2 pr-2 bg-blue-600">
         <nav className="max-w-6xl mx-auto flex items-center justify-between">
           {/* Logo */}
-          <div className='flex gap-3'>
+          <div className='flex gap-3 pt-2 pb-2'>
             <i className="fa-solid fa-person-running text-white text-3xl" />
             <h1 className="logo-text text-white text-2xl">RunningFuze</h1>
           </div>
           {/* Menu Items */}
-          <div className="space-x-6 text-white items-center hidden md:block">
-            <a href="#" className="hover:text-darkGrayishBlue">My Runs</a>
-            <a href="#" className="hover:text-darkGrayishBlue">My Workouts</a>
-            {/* Button */}
+          <div className="text-white items-center hidden md:flex">
+            <NavItems />
+            {/* Sign-out Button */}
             <button onClick={handleSignOut} className="text-white bg-red-500 p-3 rounded-xl">Sign out</button>
           </div>
-          <div className="md:hidden">
+          {/* Hamburger Menu */}
+          <div className="md:hidden pt-2 pb-2">
             <Hamburger toggled={isOpen} toggle={setOpen} color="white" />
           </div>
-          {drawerMenu}
+          <DrawerMenu setOpen={setOpen} isOpen={isOpen}/>
           {darkBackground}
         </nav>
       </header>
