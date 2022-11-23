2c1c688a9f1881ddcfc896944d6c81094a096218
Cameron <cameronpeck317@gmail.com>
2022-11-23 21:41:21 +0000

    refactor WeekChart into class component from function component #6

--- client/components/week-chart.jsx
+++ client/components/week-chart.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { createRef } from 'react';
 import { scaleBand, scaleLinear, axisLeft, axisBottom, max, select, line } from 'd3';
 
 // GitHub Ref -- https://github.com/f-kb24/d3-in-depth/blob/master/src/stories/components/Scales.tsx
@@ -45,90 +45,94 @@ const dimensions = {
   marginTop: 5
 };
 
-export default function WeekChart(props) {
+export default class WeekChart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: initialData
+    };
+    this.SVGref = createRef();
+    this.DIVref = createRef();
+  }
 
-  const ref = useRef(null);
-  const [selection, setSelection] = useState(null);
-  const [data, setData] = useState(initialData);
+  componentDidMount() {
+    const chartSVG = select(this.SVGref.current);
+    const { chartWidth, chartHeight, marginLeft, marginTop } = dimensions;
+    const { data } = this.state;
 
-  const { width, height, chartWidth, chartHeight, marginLeft, marginTop } = dimensions;
+    const x = scaleBand() // define range into uniform bands and will map it into the domain
+      .domain(data.map(d => d.name)) // takes an array of identifiers for the data
+      .range([0, chartWidth]);
 
-  const x = scaleBand() // define range into uniform bands and will map it into the domain
-    .domain(data.map(d => d.name)) // takes an array of identifiers for the data
-    .range([0, chartWidth]);
+    const y = scaleLinear() // by default domain and range is between 0 and 1
+      .domain([0, Math.floor((max(data, d => d.number) + 1))]) // takes an array [floor, ceiling]
+      .range([chartHeight, 0]);
 
-  const y = scaleLinear() // by default domain and range is between 0 and 1
-    .domain([0, Math.floor((max(data, d => d.number) + 1))]) // takes an array [floor, ceiling]
-    .range([chartHeight, 0]);
+    const xAxis = axisBottom(x)
+      .tickSizeOuter(0);
 
-  const xAxis = axisBottom(x)
-    .tickSizeOuter(0);
+    const yAxis = axisLeft(y)
+      .tickSizeOuter(0)
+      .ticks(10) // d3 tries its best but will not always give an exact number
+      .tickFormat((interval, i) => {
+        return i % 2 !== 0 ? '' : `${interval}mi`;
+      });
 
-  const yAxis = axisLeft(y)
-    .tickSizeOuter(0)
-    .ticks(10) // d3 tries its best but will not always give an exact number
-    .tickFormat((interval, i) => {
-      return i % 2 !== 0 ? '' : `${interval}mi`;
-    });
+    // Axis //
+    const xAxisGroup = chartSVG // adds the x-axis to our chart
+      .append('g')
+      .attr('transform', `translate(${marginLeft}, ${chartHeight + marginTop})`)
+      .call(xAxis);
 
-  useEffect(() => {
-    if (!selection) {
-      setSelection(select(ref.current)); // sets the current selection as our svg
-    } else {
+    xAxisGroup // formats the x-axis
+      .selectAll('text')
+      .attr('text-anchor', 'center')
+      .attr('font-size', '14px');
 
-      // Axis //
-      const xAxisGroup = selection // adds the x-axis to our chart
-        .append('g')
-        .attr('transform', `translate(${marginLeft}, ${chartHeight + marginTop})`)
-        .call(xAxis);
+    const yAxisGroup = chartSVG // adds the y-axis to our chart
+      .append('g')
+      .attr('transform', `translate(${marginLeft}, ${marginTop})`)
+      .call(yAxis);
 
-      xAxisGroup // formats the x-axis
-        .selectAll('text')
-        .attr('text-anchor', 'center')
-        .attr('font-size', '14px');
+    yAxisGroup // formats the y-axis
+      .selectAll('text')
+      .attr('text-anchor', 'center')
+      .attr('font-size', '10px');
 
-      const yAxisGroup = selection // adds the y-axis to our chart
-        .append('g')
-        .attr('transform', `translate(${marginLeft}, ${marginTop})`)
-        .call(yAxis);
+    // Line //
+    chartSVG
+      .append('path')
+      .data([data])
+      .attr('class', 'line')
+      .attr('fill', 'none')
+      .attr('transform', `translate(${marginLeft + 23.5}, ${marginTop})`) // transforms entire graph
+      .attr('stroke-width', 2.0)
+      .attr('stroke', 'steelblue')
+      .attr('d', line()
+        .x(d => { return x(d.name); })
+        .y(d => { return y(d.number); })
+      );
 
-      yAxisGroup // formats the y-axis
-        .selectAll('text')
-        .attr('text-anchor', 'center')
-        .attr('font-size', '10px');
+    // Data Points //
+    chartSVG
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('transform', `translate(${marginLeft + 23.5}, ${marginTop})`) // transforms entire graph
+      .attr('fill', 'red')
+      .attr('stroke', 'none')
+      .attr('cx', function (d) { return x(d.name); })
+      .attr('cy', function (d) { return y(d.number); })
+      .attr('r', 3);
+  }
 
-      // Line //
-      selection
-        .append('path')
-        .data([data])
-        .attr('class', 'line')
-        .attr('fill', 'none')
-        .attr('transform', `translate(${marginLeft + 23.5}, ${marginTop})`) // transforms entire graph
-        .attr('stroke-width', 2.0)
-        .attr('stroke', 'steelblue')
-        .attr('d', line()
-          .x(function (d) { return x(d.name); })
-          .y(function (d) { return y(d.number); })
-        );
-
-      // Data Points //
-      selection
-        .selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('transform', `translate(${marginLeft + 23.5}, ${marginTop})`) // transforms entire graph
-        .attr('fill', 'red')
-        .attr('stroke', 'none')
-        .attr('cx', function (d) { return x(d.name); })
-        .attr('cy', function (d) { return y(d.number); })
-        .attr('r', 3);
-    }
-  }, [selection]);
-
-  return (
-    <div>
-      <svg ref={ref} width={width} height={height} />
-    </div>
-  );
+  render() {
+    const { width, height } = dimensions;
+    return (
+      <div ref={this.DIVref}>
+        <svg ref={this.SVGref} width={width} height={height} />
+      </div>
+    );
+  }
 }
