2438034fbdaf57b7a4760940ae5a54c2ee24f644
Cameron <cameronpeck317@gmail.com>
2022-11-23 18:04:50 +0000

    add path element for line in line chart and run line through datapoints on the chart via d3 #6

--- client/components/week-chart.jsx
+++ client/components/week-chart.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { scaleBand, scaleLinear, axisLeft, axisBottom, max, select } from 'd3';
+import { scaleBand, scaleLinear, axisLeft, axisBottom, max, select, line } from 'd3';
 
 // GitHub Ref -- https://github.com/f-kb24/d3-in-depth/blob/master/src/stories/components/Scales.tsx
 // for updates -- > use https://www.youtube.com/watch?v=OM_6sbgqrHc&list=PLzCAqE_rafAc_2QWK8ii16m2ju4qhYAJT&index=5
@@ -8,7 +8,7 @@ import { scaleBand, scaleLinear, axisLeft, axisBottom, max, select } from 'd3';
 const initialData = [
   {
     name: 'Sun',
-    number: 7.22
+    number: 4.22
   },
   {
     name: 'Mon',
@@ -24,7 +24,7 @@ const initialData = [
   },
   {
     name: 'Thu',
-    number: 0.00
+    number: 2.20
   },
   {
     name: 'Fri',
@@ -38,10 +38,11 @@ const initialData = [
 
 const dimensions = {
   width: 500,
-  height: 430,
-  chartWidth: 400,
-  chartHeight: 400,
-  marginLeft: 40
+  height: 375,
+  chartWidth: 330,
+  chartHeight: 350,
+  marginLeft: 25,
+  marginTop: 5
 };
 
 export default function WeekChart(props) {
@@ -50,70 +51,84 @@ export default function WeekChart(props) {
   const [selection, setSelection] = useState(null);
   const [data, setData] = useState(initialData);
 
-  const { width, height, chartWidth, chartHeight, marginLeft } = dimensions;
+  const { width, height, chartWidth, chartHeight, marginLeft, marginTop } = dimensions;
 
   const x = scaleBand() // define range into uniform bands and will map it into the domain
     .domain(data.map(d => d.name)) // takes an array of identifiers for the data
-    .range([0, chartWidth])
-    .paddingInner(0.05); // closer to 1 --> less padding
+    .range([0, chartWidth]);
 
-  const y = scaleLinear() // by default domain and range is between 0 and one
-    .domain([0, max(data, d => d.number)]) // takes an array [floor, ceiling]
+  const y = scaleLinear() // by default domain and range is between 0 and 1
+    .domain([0, Math.floor((max(data, d => d.number) + 1))]) // takes an array [floor, ceiling]
     .range([chartHeight, 0]);
 
-  const xAxis = axisBottom(x);
+  const xAxis = axisBottom(x)
+    .tickSizeOuter(0);
+
   const yAxis = axisLeft(y)
-    .ticks(20) // d3 tries its best but will not always give an exact number
-    .tickFormat(d => `${d}mi`); // takes a function where the arg is tick text --> return is what you see on the chart
+    .tickSizeOuter(0)
+    .ticks(30) // d3 tries its best but will not always give an exact number
+    .tickFormat((interval, i) => {
+      return i % 5 !== 0 ? '' : `${interval}mi`;
+    });
 
   useEffect(() => {
     if (!selection) {
-      setSelection(select(ref.current));
+      setSelection(select(ref.current)); // sets the current selection as our svg
     } else {
+
       // Axis //
-      const xAxisGroup = selection
+      const xAxisGroup = selection // adds the x-axis to our chart
         .append('g')
-        .attr('transform', `translate(${marginLeft}, ${chartHeight})`)
+        .attr('transform', `translate(${marginLeft}, ${chartHeight + marginTop})`)
         .call(xAxis);
 
-      xAxisGroup
+      xAxisGroup // formats the x-axis
         .selectAll('text')
         .attr('text-anchor', 'center')
-        .attr('font-size', '15px');
+        .attr('font-size', '12px');
 
-      const yAxisGroup = selection
+      const yAxisGroup = selection // adds the y-axis to our chart
         .append('g')
-        .attr('transform', `translate(${marginLeft}, 0)`)
+        .attr('transform', `translate(${marginLeft}, ${marginTop})`)
         .call(yAxis);
 
-      yAxisGroup
+      yAxisGroup // formats the y-axis
         .selectAll('text')
         .attr('text-anchor', 'center')
         .attr('font-size', '10px');
 
-      // Bars //
+      // Line //
       selection
-        .append('g') // wrap in a group so that we can apply trasformations later
-        .attr('transform', `translate(${marginLeft})`) // transforms entire graph
-        .selectAll('rect')
+        .append('path')
+        .data([data])
+        .attr('class', 'line')
+        .attr('fill', 'none')
+        .attr('transform', `translate(${marginLeft + 23.5}, ${marginTop})`) // transforms entire graph
+        .attr('stroke-width', 2.0)
+        .attr('stroke', 'steelblue')
+        .attr('d', line()
+          .x(function (d) { return x(d.name); })
+          .y(function (d) { return y(d.number); })
+        );
+
+      // Data Points //
+      selection
+        .selectAll('circle')
         .data(data)
         .enter()
-        .append('rect')
-        .attr('width', x.bandwidth) // x.bandwith fills the width to the appropriate range with no default padding
-        .attr('height', 0)
-        .attr('y', chartHeight)
-        .transition()
-        .duration(500)
-        .attr('height', d => chartHeight - y(d.number))
-        .attr('x', d => x(d.name))
-        .attr('y', d => y(d.number))
-        .attr('fill', 'orange');
+        .append('circle')
+        .attr('transform', `translate(${marginLeft + 23.5}, ${marginTop})`) // transforms entire graph
+        .attr('fill', 'red')
+        .attr('stroke', 'none')
+        .attr('cx', function (d) { return x(d.name); })
+        .attr('cy', function (d) { return y(d.number); })
+        .attr('r', 3);
     }
   }, [selection]);
 
   return (
     <div>
-      <svg ref={ref} width='100%' height='100%' viewBox={`0, 0, ${Math.min(width, height)}, ${Math.min(width, height)} `}/>
+      <svg ref={ref} width='100%' height='100%' viewBox={`0, 0, ${Math.min(width, height)}, ${Math.min(width, height)}`}/>
     </div>
   );
 }
