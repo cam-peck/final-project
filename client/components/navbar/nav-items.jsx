3f4280d202d8ebfa1da5c2c7d6ebb1f717e9a92b
Cameron <cameronpeck317@gmail.com>
2022-12-06 21:15:40 +0000

    fix incorrect button click area on my runs button and remove unneeded workout button

--- client/components/navbar/nav-items.jsx
+++ client/components/navbar/nav-items.jsx
@@ -25,21 +25,17 @@ export default class NavItems extends React.Component {
       ? 'md:flex'
       : '';
     return (
-      <>
-        <div className="flex items-center hover:cursor-pointer p-4 m-2 transition-all ease-in-out duration-200 hover:bg-gray-400 hover:bg-opacity-20">
+      <div className="flex transition-all ease-in-out duration-200 relative z-0">
+        <button onClick={handleClick} className="flex items-center text-lg p-4 m-2 mr-6 transition-all ease-in-out duration-200 hover:bg-gray-400 hover:bg-opacity-20">
           <i className="fa-solid fa-person-running text-2xl pr-3" />
-          <button onClick={handleClick} className="text-lg">My Runs</button>
-          <div onClick={handleClick} className={`hidden absolute top-16 ${showMyRuns} z-10 flex-col bg-gray-100 text-black rounded-sm shadow-md`}>
-            <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=progress">Progress</a>
-            <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=activities">Activities</a>
-            <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=profile">Profile</a>
-          </div>
+          My Runs
+        </button>
+        <div onClick={handleClick} className={`hidden absolute top-16 left-2 ${showMyRuns} z-10 flex-col bg-gray-100 text-black rounded-sm shadow-md`}>
+          <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=progress">Progress</a>
+          <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=activities">Activities</a>
+          <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=profile">Profile</a>
         </div>
-        <div className="flex items-center hover:cursor-pointer p-4 m-2 transition-all ease-in-out duration-200 hover:bg-gray-400 hover:bg-opacity-20">
-          <i className="fa-solid fa-dumbbell text-lg pr-3" />
-          <a href="#sign-in" className="text-lg">My Workouts</a>
-        </div>
-      </>
+      </div>
     );
   }
 }
