f5dd07ee5c931d639684029694882c03b94a71dd
Cameron <cameronpeck317@gmail.com>
2022-12-07 15:50:07 +0000

    add full-width to forms and min-width css to date inputs to increase width on mobile displays

--- client/components/inputs/date-input.jsx
+++ client/components/inputs/date-input.jsx
@@ -7,7 +7,7 @@ export default function DateInput(props) {
   return (
     <div className="mb-4 flex flex-col">
       <label className={`${showLabel} font-lora font-medium text-md pb-2`} htmlFor={props.name}>{props.label}</label>
-      <input autoComplete="on" type={props.type} name={props.name} id={props.name} value={props.value} min={props.dateMin} max={props.dateMax} onChange={props.onChange} className="w-full rounded-lg px-3 py-3.5 border border-gray-300 focus:outline-blue-500" placeholder={props.placeholder} required />
+      <input autoComplete="on" type={props.type} name={props.name} id={props.name} value={props.value} min={props.dateMin} max={props.dateMax} onChange={props.onChange} className="min-w-full rounded-lg px-3 py-3.5 border border-gray-300 focus:outline-blue-500" placeholder={props.placeholder} required />
     </div>
   );
 }
