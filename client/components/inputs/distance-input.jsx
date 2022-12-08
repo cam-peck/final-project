e46fab936ca242d4b9bc3e0177e160fa8a44b61c
Cameron <cameronpeck317@gmail.com>
2022-12-08 19:20:13 +0000

    fix styling issue with distance and duration inputs so that they also change to a lightblue on hover / when typing

--- client/components/inputs/distance-input.jsx
+++ client/components/inputs/distance-input.jsx
@@ -5,7 +5,7 @@ export default function DistanceInput(props) {
     <fieldset className="flex mb-4">
       <legend className="font-lora font-md text-md font-medium pb-2">Distance</legend>
       <div className="w-full">
-        <input type="number" name="distance" min="0.01" max="999" step="0.01" value={props.distanceValue} onChange={props.onChange} className="w-full px-3 py-3.5 text-center border border-gray-300 text-gray-900 rounded-lg rounded-r-none" placeholder="0.00" required />
+        <input type="number" name="distance" min="0.01" max="999" step="0.01" value={props.distanceValue} onChange={props.onChange} className="w-full px-3 py-3.5 text-center border border-gray-300 focus:outline-blue-500 text-gray-900 rounded-lg rounded-r-none" placeholder="0.00" required />
       </div>
       <div className="w-32 flex-shrink-0">
         <select name="distanceUnits" value={props.distanceTypeValue} onChange={props.onChange} className="w-full bg-white border border-gray-400 hover:border-gray-500 px-3 h-[54px] rounded-tr-lg rounded-br-lg shadow focus:outline-none focus:shadow-outline">
