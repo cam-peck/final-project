cec996a3ded10c26ebf5f0400b1b9d48b85f2e5a
Cameron <cameronpeck317@gmail.com>
2022-12-19 16:54:46 +0000

    on componentDidMount fetch all of the runs for the currently logged in user and pass them into the WorkoutCard component via map #8

--- client/components/my-workouts.jsx
+++ client/components/my-workouts.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import WorkoutCard from './cards/workout-card';
 import TextInput from './inputs/text-input';
+import NetworkError from './network-error';
+import LoadingSpinner from './loading-spinner';
+import { AppContext } from '../lib';
 
 export default class MyWorkouts extends React.Component {
   constructor(props) {
@@ -13,10 +16,42 @@ export default class MyWorkouts extends React.Component {
   }
 
   componentDidMount() {
-    console.log('component mounted: fetching workout data');
+    this.setState({
+      fetchingData: true
+    }, () => {
+      const { user } = this.context;
+      const req = {
+        method: 'GET',
+        headers: {
+          'X-Access-Token': localStorage.getItem('runningfuze-project-jwt')
+        },
+        user
+      };
+      fetch('/api/workouts', req)
+        .then(response => response.json())
+        .then(result => {
+          this.setState({
+            workoutData: result,
+            fetchingData: false
+          });
+        })
+        .catch(error => {
+          console.error('There was an error!', error);
+          this.setState({
+            networkError: true
+          });
+        });
+    });
   }
 
   render() {
+    if (this.state.networkError) {
+      return <NetworkError />;
+    }
+    if (this.state.fetchingData) {
+      return <LoadingSpinner />;
+    }
+    const { workoutData } = this.state;
     return (
       <>
         <div>
@@ -24,13 +59,10 @@ export default class MyWorkouts extends React.Component {
           <TextInput placeholder="Search by title, description, distance-type, or date..." type="text" name="searchbar" id="searchbar"/>
         </div>
         <div className="flex flex-col gap-6 md:grid md:grid-cols-2 md:gap-8">
-          <WorkoutCard />
-          <WorkoutCard />
-          <WorkoutCard />
-          <WorkoutCard />
-          <WorkoutCard />
+          { workoutData.map((workout, index) => { return <WorkoutCard key={index} data={workout}/>; })}
         </div>
       </>
     );
   }
 }
+MyWorkouts.contextType = AppContext;
