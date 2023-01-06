827741ac47e2441f4359d616786d6a86a1938b6e
Cameron <cameronpeck317@gmail.com>
2023-01-06 21:20:20 +0000

    add NoGpxFound component for when GPX data is not uploaded and use in both UploadedRunCard and MainRunCard components #22

--- client/components/cards/upload-run-card.jsx
+++ client/components/cards/upload-run-card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Map from '../gmaps/map';
+import NoGpxFound from '../gmaps/no-gpx-found';
 
 export default function UploadRunCard(props) {
   const { fileInputRef, toggleGpxTrue, handleGpxData, gpxPath, hasGpx } = props;
@@ -9,17 +10,12 @@ export default function UploadRunCard(props) {
         {
           hasGpx === true
             ? <Map gpxPath={gpxPath} />
-            : <div className="relative">
-              <img className="object-cover w-full h-72 md:h-80 rounded-tr-xl rounded-tl-xl" src='/images/placeholder-map.png' alt='placeholder-map' />
-              <div className="bg-black absolute h-72 md:h-80 w-full top-0 opacity-60 rounded-tr-xl rounded-tl-xl flex justify-center items-center">
-                <h1 className='text-white text-xl'>No GPS data found...</h1>
-              </div>
-            </div>
+            : <NoGpxFound borderRounded="rounded-tr-xl rounded-tl-xl" height="h-72 md:h-80" width="w-full"/>
         }
       </div>
       <div className="bg-blue-200 flex justify-between items-center rounded-br-xl rounded-bl-xl px-5 py-3">
         <p className="font-lora text-lg">Have GPS data?</p>
-        <label htmlFor="file-input" className="bg-blue-600 text-white px-7 py-3 rounded-3xl hover:cursor-pointer active:transform active:translate-y-[1px]">Upload data</label>
+        <label htmlFor="file-input" className="bg-blue-600 text-white px-7 py-3 rounded-3xl hover:cursor-pointer active:transform active:translate-y-[1px] select-none">Upload data</label>
         <input id="file-input" type="file" name="file" accept=".gpx" onChange={event => { toggleGpxTrue(); handleGpxData(event); }} className="hidden" ref={fileInputRef}/>
       </div>
     </section>
---
+++ client/components/gmaps/no-gpx-found.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NoGpxFound(props) {
+  const { borderRounded, height, width } = props;
+  return (
+    <div className="relative">
+      <img className={`object-cover ${width} ${height} ${borderRounded}`} src='/images/gps-map-placeholder.png' alt='placeholder-map' />
+      <div className={`bg-black absolute top-0 opacity-70 ${width} ${height} ${borderRounded} flex justify-center items-center`}>
+        <h1 className='text-white text-xl'>No GPS data found...</h1>
+      </div>
+    </div>
+  );
+}
