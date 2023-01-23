a476afc4ecc5e1c65f60e9bcd39151a62c9cc879
Cameron <cameronpeck317@gmail.com>
2023-01-23 22:44:39 +0000

    add upcoming-workouts box to progress component and ensure workouts are rendered correctly. Style similarly to other cards on My Progress page

--- client/components/runs/progress.jsx
+++ client/components/runs/progress.jsx
@@ -4,12 +4,14 @@ import ProgressSquares from '../progress-squares/progress-squares';
 import WeeklyRunChart from '../week-progress-chart/weekly-run-chart';
 import LoadingSpinner from '../loading-spinner';
 import NetworkError from '../network-error';
+import WorkoutCard from '../cards/workout-card';
 
 export default class Progress extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       yearlyRunData: [],
+      workoutData: [],
       fetchingData: true,
       networkError: false
     };
@@ -25,10 +27,13 @@ export default class Progress extends React.Component {
       user
     };
     try {
-      const response = await fetch('/api/progress', req);
-      const result = await response.json();
+      const progressResponse = await fetch('/api/progress', req);
+      const progressResult = await progressResponse.json();
+      const workoutResponse = await fetch('/api/workouts', req);
+      const workoutResult = await workoutResponse.json();
       this.setState({
-        yearlyRunData: result,
+        yearlyRunData: progressResult,
+        workoutData: workoutResult,
         fetchingData: false
       });
     } catch (err) {
@@ -44,7 +49,7 @@ export default class Progress extends React.Component {
     if (this.state.fetchingData) {
       return <LoadingSpinner />;
     }
-    const { yearlyRunData } = this.state;
+    const { yearlyRunData, workoutData } = this.state;
     return (
       <section className="pl-6 pr-6 max-w-6xl mx-auto mt-6 mb-4">
         <h1 className="font-lora font-medium text-2xl mb-6">My Progress</h1>
@@ -52,10 +57,15 @@ export default class Progress extends React.Component {
           <ProgressSquares data={yearlyRunData}/>
         </div>
         <div className="flex flex-col md:flex-row">
-          <div className='w-full md:pr-4 md:w-6/12 lg:w-5/12'>
+          <div className='w-full md:pr-4 md:w-6/12 lg:w-6/12 mb-4'>
             <WeeklyRunChart data={yearlyRunData}/>
           </div>
-          <div className='w-6/12' />
+          <div className='w-full md:w-6/12 lg:w-6/12 h-[372px] overflow-y-scroll bg-white pl-6 pr-6 pt-5 pb-5 rounded-xl border border-gray-300 shadow-sm'>
+            <h1 className="font-lora text-xl font-medium mb-6">Upcoming Workouts</h1>
+            {
+              workoutData.map(workout => <div className="mb-4" key={workout.workoutId}> <WorkoutCard data={workout} /> </div>)
+            }
+          </div>
         </div>
       </section>
     );
