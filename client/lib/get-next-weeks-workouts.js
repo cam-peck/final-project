7a2fb0cbb1ce815c81cac9093197ce7e28bfca61
Cameron <cameronpeck317@gmail.com>
2023-02-16 19:06:44 +0000

    fix bug where the end date was one day off due to removing the tz data and forcing the time to midnight. Fix bug where users could not select future dates for workouts -- put max at 1 year forward

--- client/lib/get-next-weeks-workouts.js
+++ client/lib/get-next-weeks-workouts.js
@@ -4,11 +4,11 @@ import removeTz from './remove-tz';
 export default function getNextWeeksWorkouts(workoutData) {
   const nextWeeksWorkouts = [];
   const start = subDays(new Date(), 1); // yesterday, so that we include today
-  const end = addDays(start, 7); // one week from today
+  const end = addDays(start, 8); // up to one week from today
   workoutData.forEach(workout => {
     const workoutDate = new Date(removeTz(workout.date));
     if (isAfter(workoutDate, start) && isBefore(workoutDate, end)) {
-      nextWeeksWorkouts.push(workout);
+      nextWeeksWorkouts.unshift(workout);
     }
   });
   return nextWeeksWorkouts;
