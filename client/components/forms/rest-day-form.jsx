3f49405689b14166a8e2c4efc575911c3e58f6e1
Cameron <cameronpeck317@gmail.com>
2023-02-22 21:39:47 +0000

    rename rest-days component to rest-day-form and move to forms folder. remove lots of unneccessary html

--- client/components/forms/rest-day-form.jsx
+++ client/components/forms/rest-day-form.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { removeTz } from '../../lib';
-import { format } from 'date-fns';
+import { format, subYears } from 'date-fns';
+import WeekdaySelector from '../inputs/weekday-selector';
+import DatePicker from 'react-datepicker';
 
-export default function RestDays(props) {
+export default function RestDayForm(props) {
   const { restData } = props;
   return (
-    <>
+    <section>
+      <WeekdaySelector />
       <label className="font-lora font-medium text-md" htmlFor='rest-date-picker'>Custom Rest Days</label>
       <div className="grid grid-cols-1 x2s:grid-cols-2 sm:grid-cols-3 gap-3">
         {restData.map(restDay => {
@@ -29,6 +32,7 @@ export default function RestDays(props) {
           } else return '';
         })}
       </div>
-    </>
+      <DatePicker className="w-full rounded-lg px-3 py-3.5 border border-gray-300 focus:outline-blue-500 mb-4 -mt-2" dateFormat='MM/dd/yyy' maxDate={subYears(new Date(), 10)} minDate={subYears(new Date(), 100)} placeholderText='Click to add a custom date.' id='rest-date-picker' />
+    </section>
   );
 }
--- client/components/progress-squares/rest-day-modal.jsx
+++ client/components/progress-squares/rest-day-modal.jsx
@@ -1,46 +1,43 @@
 import React from 'react';
-import DatePicker from 'react-datepicker';
-import RestDays from './rest-days';
-import { subYears } from 'date-fns';
-
+import RestDayForm from '../forms/rest-day-form';
 import 'react-datepicker/dist/react-datepicker.css';
-import WeekdaySelector from '../inputs/weekday-selector';
+
+// TODOs
+// 1: Lots of form hooking up on the Rest Days modal for form controls -- where to keep in state?
+// 2: Clean up Custom Rest Days so that spacing is correct and color scheme looks better
+// 3: Add delete functionality (or an issue for it) for rest days that are already added -- add a snackbar for confirmation
 
 export default function RestDayModal(props) {
   const { closeModal, restData } = props;
   return (
-    <div onClick={event => { if (event.target.id === 'background') { closeModal(); } }} id="background" className="w-full overflow-y-scroll h-screen fixed flex justify-center items-center top-0 left-0 bg-gray-800 bg-opacity-30 z-10">
-      <div className="absolute tall:relative bg-white rounded-xl p-6 max-w-xl min-w-[260px] w-[85%] top-10 tall:top-0 ml-6 mr-6">
-        <button onClick={closeModal} className="absolute -top-4 -right-5 w-10 h-10 rounded-full bg-red-600 text-white"><i className="fa-regular fa-xl fa-circle-xmark" /></button>
-        {/* content */}
-        <div className="pl-1 mt-2">
+    <form>
+      <div onClick={event => { if (event.target.id === 'background') { closeModal(); } }} id="background" className="w-full overflow-y-scroll h-screen fixed flex justify-center items-center top-0 left-0 bg-gray-800 bg-opacity-30 z-10">
+        <div className="absolute tall:relative bg-white rounded-xl p-6 max-w-xl min-w-[260px] w-[85%] top-10 tall:top-0 ml-6 mr-6">
+          <button onClick={closeModal} className="absolute -top-4 -right-5 w-10 h-10 rounded-full bg-red-600 text-white"><i className="fa-regular fa-xl fa-circle-xmark" /></button>
+          {/* content */}
+          <div className="pl-1 mt-2">
 
-          {/* content-header */}
-          <div className="mb-4">
-            <div className="flex justify-between items-center relative mb-1">
-              <h1 className="font-lora text-xl font-bold">Rest Days</h1>
-            </div>
-          </div>
-
-          {/* content-main */}
-          <div className="font-roboto text-md max-w-lg mb-4">
-            <div className="mb-4 flex flex-col gap-4">
-              <WeekdaySelector />
-              <div className="w-full flex flex-col gap-4">
-                <section>
-                  <RestDays restData={restData}/>
-                </section>
-                <section>
-                  <DatePicker className="w-full rounded-lg px-3 py-3.5 border border-gray-300 focus:outline-blue-500 mb-4 -mt-2" dateFormat='MM/dd/yyy' maxDate={subYears(new Date(), 10)} minDate={subYears(new Date(), 100)} placeholderText='Click to add a custom date.' id='rest-date-picker'/>
-                </section>
+            {/* content-header */}
+            <div className="mb-4">
+              <div className="flex justify-between items-center relative mb-1">
+                <h1 className="font-lora text-xl font-bold">Rest Days</h1>
               </div>
             </div>
-          </div>
-          <div className="font-roboto text-md max-w-lg mb-4">
-            <p>Save</p>
+
+            {/* content-main */}
+            <div className="font-roboto text-md max-w-lg mb-4">
+              <div className="mb-4 flex flex-col gap-4">
+                <div className="w-full flex flex-col gap-4">
+                  <RestDayForm restData={restData}/>
+                </div>
+              </div>
+            </div>
+            <div className="font-roboto text-md max-w-lg mb-4">
+              <p>Save</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
