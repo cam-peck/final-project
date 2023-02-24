7a4c8462baddb1a3f884af75d1bab67870783477
Cameron <cameronpeck317@gmail.com>
2023-02-24 20:56:48 +0000

    refactor progress component to react hooks

--- client/components/runs/progress.jsx
+++ client/components/runs/progress.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { AppContext, getNextWeeksWorkouts } from '../../lib';
 import ProgressSquares from '../progress-squares/progress-squares';
 import WeeklyRunChart from '../week-progress-chart/weekly-run-chart';
@@ -6,79 +6,71 @@ import LoadingSpinner from '../loading-spinner';
 import NetworkError from '../network-error';
 import WorkoutCard from '../cards/workout-card';
 
-export default class Progress extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      yearlyRunData: [],
-      yearlyRestData: [],
-      workoutData: [],
-      fetchingData: true,
-      networkError: false
-    };
-    this.toggleNetworkError = this.toggleNetworkError.bind(this);
+export default function Progress(props) {
+  const [yearlyRunData, setYearlyRunData] = useState([]);
+  const [yearlyRestData, setYearlyRestData] = useState([]);
+  const [workoutData, setYearlyWorkoutData] = useState([]);
+  const [fetchingData, setFetchingData] = useState(true);
+  const [networkError, setNetworkError] = useState(false);
+
+  const { user } = useContext(AppContext);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = {
+        method: 'GET',
+        headers: {
+          'X-Access-Token': localStorage.getItem('runningfuze-project-jwt')
+        },
+        user
+      };
+      try {
+        const progressResponse = await fetch('/api/progress', req);
+        const progressResult = await progressResponse.json();
+        const workoutResponse = await fetch('/api/workouts', req);
+        const workoutResult = await workoutResponse.json();
+        setYearlyRunData(progressResult.runDates);
+        setYearlyWorkoutData(workoutResult);
+        setFetchingData(false);
+      } catch (err) {
+        console.error('An error occured!', err);
+        setNetworkError(true);
+      }
+    }
+    fetchData();
+  });
+
+  const toggleNetworkError = e => {
+    setNetworkError(true);
+  };
+
+  if (networkError) {
+    return <NetworkError />;
+  }
+  if (fetchingData) {
+    return <LoadingSpinner />;
   }
 
-  async componentDidMount() {
-    const { user } = this.context;
-    const req = {
-      method: 'GET',
-      headers: {
-        'X-Access-Token': localStorage.getItem('runningfuze-project-jwt')
-      },
-      user
-    };
-    try {
-      const progressResponse = await fetch('/api/progress', req);
-      const progressResult = await progressResponse.json();
-      const workoutResponse = await fetch('/api/workouts', req);
-      const workoutResult = await workoutResponse.json();
-      this.setState({
-        yearlyRunData: progressResult.runDates,
-        workoutData: workoutResult,
-        fetchingData: false
-      });
-    } catch (err) {
-      console.error('An error occured!', err);
-      this.setState({ networkError: true });
-    }
-  }
-
-  toggleNetworkError() {
-    this.setState({ networkError: !this.state.networkError });
-  }
-
-  render() {
-    if (this.state.networkError) {
-      return <NetworkError />;
-    }
-    if (this.state.fetchingData) {
-      return <LoadingSpinner />;
-    }
-    const { yearlyRunData, workoutData } = this.state;
-    const { toggleNetworkError } = this;
-    const nextWeeksWorkouts = getNextWeeksWorkouts(workoutData);
-    return (
-      <section className="pl-6 pr-6 max-w-6xl mx-auto mt-6 mb-4">
-        <h1 className="font-lora font-medium text-2xl mb-6">My Progress</h1>
-        <div className='mb-4'>
-          <ProgressSquares runData={yearlyRunData} toggleNetworkError={toggleNetworkError}/>
+  const nextWeeksWorkouts = getNextWeeksWorkouts(workoutData);
+  return (
+    <section className="pl-6 pr-6 max-w-6xl mx-auto mt-6 mb-4">
+      <h1 className="font-lora font-medium text-2xl mb-6">My Progress</h1>
+      <div className='mb-4'>
+        <ProgressSquares runData={yearlyRunData} toggleNetworkError={toggleNetworkError}/>
+      </div>
+      <div className="flex flex-col md:flex-row">
+        <div className='w-full md:pr-4 md:w-6/12 lg:w-6/12 mb-4'>
+          <WeeklyRunChart data={yearlyRunData}/>
         </div>
-        <div className="flex flex-col md:flex-row">
-          <div className='w-full md:pr-4 md:w-6/12 lg:w-6/12 mb-4'>
-            <WeeklyRunChart data={yearlyRunData}/>
-          </div>
-          <div className='w-full md:w-6/12 lg:w-6/12 h-[372px] overflow-y-scroll bg-white pl-6 pr-6 pt-5 pb-5 rounded-xl border border-gray-300 shadow-sm'>
-            <h1 className="font-lora text-xl font-medium mb-6">Upcoming Workouts</h1>
-            {
-              nextWeeksWorkouts.length === 0
-                ? <div className= "flex justify-center items-center h-60" ><p className="text-center italic">No workouts found for the next 7 days...</p></div>
-                : nextWeeksWorkouts.map(workout => <div className="mb-4" key={workout.workoutId}> <WorkoutCard data={workout} editDeleteEnabled={false} /> </div>)
-            }
-          </div>
+        <div className='w-full md:w-6/12 lg:w-6/12 h-[372px] overflow-y-scroll bg-white pl-6 pr-6 pt-5 pb-5 rounded-xl border border-gray-300 shadow-sm'>
+          <h1 className="font-lora text-xl font-medium mb-6">Upcoming Workouts</h1>
+          {
+            nextWeeksWorkouts.length === 0
+              ? <div className= "flex justify-center items-center h-60" ><p className="text-center italic">No workouts found for the next 7 days...</p></div>
+              : nextWeeksWorkouts.map(workout => <div className="mb-4" key={workout.workoutId}> <WorkoutCard data={workout} editDeleteEnabled={false} /> </div>)
+          }
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
-Progress.contextType = AppContext;
