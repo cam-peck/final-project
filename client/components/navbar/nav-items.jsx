d32f27495e354aeb6a1fa8d57d7daec6b5f72428
Cameron <cameronpeck317@gmail.com>
2023-03-01 18:25:04 +0000

    refactor navItems to react hooks

--- client/components/navbar/nav-items.jsx
+++ client/components/navbar/nav-items.jsx
@@ -1,55 +1,46 @@
-import React from 'react';
-export default class NavItems extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      myRunsNav: false,
-      myWorkoutsNav: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+import React, { useState } from 'react';
 
-  handleClick(event) {
-    if (event.target.tagName === 'BUTTON' && this.state.myRunsNav === false) {
-      if (this.props.drawerIsOpen) {
+export default function NavItems(props) {
+  const { drawerIsOpen } = props;
+
+  const [myRunsNavIsOpen, setMyRunsNavIsOpen] = useState(false);
+
+  const handleClick = event => {
+    if (event.target.tagName === 'BUTTON' && myRunsNavIsOpen === false) {
+      if (drawerIsOpen) {
         window.location.hash = '#home?tab=activities';
       } else {
-        this.setState({ myRunsNav: true });
+        setMyRunsNavIsOpen(true);
       }
     } else {
-      this.setState({ myRunsNav: false });
+      setMyRunsNavIsOpen(false);
     }
-  }
+  };
 
-  render() {
-    const { myRunsNav } = this.state;
-    const { drawerIsOpen } = this.props;
-    const { handleClick } = this;
-    const showMyRuns = myRunsNav === true
-      ? 'md:flex'
-      : '';
-    const myRunsButton = drawerIsOpen === true
-      ? 'w-full p-4 pl-3.5'
-      : 'p-4 m-2 mr-4';
-    const myWorkoutsNav = drawerIsOpen === true
-      ? 'w-full p-4 pl-3'
-      : 'p-4 m-2 mr-6';
-    return (
-      <div className="flex transition-all ease-in-out duration-200 relative z-10 flex-col md:flex-row">
-        <button onClick={handleClick} className={`flex items-center text-lg ${myRunsButton} transition-all ease-in-out duration-200 hover:bg-gray-400 hover:bg-opacity-20`}>
-          <i className="fa-solid fa-person-running text-2xl pr-3" />
-          My Runs
-        </button>
-        <div onClick={handleClick} className={`hidden absolute top-16 left-2 ${showMyRuns} z-10 flex-col bg-gray-100 text-black rounded-sm shadow-md`}>
-          <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=progress">Progress</a>
-          <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=activities">Activities</a>
-          <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=profile">Profile</a>
-        </div>
-        <a href='#workouts' className={`flex items-center text-lg transition-all ease-in-out duration-200 hover:bg-gray-400 hover:bg-opacity-20 ${myWorkoutsNav}`}>
-          <i className="fa-solid fa-dumbbell text-lg pr-3" />
-          My Workouts
-        </a>
+  const showMyRuns = myRunsNavIsOpen === true
+    ? 'md:flex'
+    : 'hidden';
+  const myRunsButton = drawerIsOpen === true
+    ? 'w-full p-4 pl-3.5'
+    : 'p-4 m-2 mr-4';
+  const myWorkoutsNav = drawerIsOpen === true
+    ? 'w-full p-4 pl-3'
+    : 'p-4 m-2 mr-6';
+  return (
+    <div className="flex transition-all ease-in-out duration-200 relative z-10 flex-col md:flex-row">
+      <button onClick={handleClick} className={`flex items-center text-lg ${myRunsButton} transition-all ease-in-out duration-200 hover:bg-gray-400 hover:bg-opacity-20`}>
+        <i className="fa-solid fa-person-running text-2xl pr-3" />
+        My Runs
+      </button>
+      <div onClick={handleClick} className={`absolute top-16 left-2 ${showMyRuns} z-10 flex-col bg-gray-100 text-black rounded-sm shadow-md`}>
+        <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=progress">Progress</a>
+        <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=activities">Activities</a>
+        <a className="hover:bg-blue-300 w-40 py-4 text-center" href="#home?tab=profile">Profile</a>
       </div>
-    );
-  }
+      <a href='#workouts' className={`flex items-center text-lg transition-all ease-in-out duration-200 hover:bg-gray-400 hover:bg-opacity-20 ${myWorkoutsNav}`}>
+        <i className="fa-solid fa-dumbbell text-lg pr-3" />
+        My Workouts
+      </a>
+    </div>
+  );
 }
