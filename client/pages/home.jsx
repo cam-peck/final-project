4432c7d1f6af777b05dc717c5b037a39c2aad431
Cameron <cameronpeck317@gmail.com>
2023-03-01 19:52:56 +0000

    refactor home runs and workout to react hooks

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import RunTabs from '../components/runs/run-tabs';
 import Progress from '../components/runs/progress';
 import Activities from '../components/runs/activities';
@@ -6,24 +6,24 @@ import Profile from '../components/runs/profile';
 import Redirect from '../components/redirect';
 import { AppContext } from '../lib';
 
-export default class Home extends React.Component {
-  render() {
-    if (!this.context.user) return <Redirect to='sign-in' />;
-    return (
-      <main>
-        <RunTabs tab={this.props.tab}>
-          <div label="progress">
-            <Progress />
-          </div>
-          <div label="activities">
-            <Activities />
-          </div>
-          <div label="profile">
-            <Profile />
-          </div>
-        </RunTabs>
-      </main>
-    );
-  }
+export default function Home(props) {
+  const { user } = useContext(AppContext);
+  const { tab } = props;
+
+  if (!user) return <Redirect to='sign-in' />;
+  return (
+    <main>
+      <RunTabs tab={tab}>
+        <div label="progress">
+          <Progress />
+        </div>
+        <div label="activities">
+          <Activities />
+        </div>
+        <div label="profile">
+          <Profile />
+        </div>
+      </RunTabs>
+    </main>
+  );
 }
-Home.contextType = AppContext;
