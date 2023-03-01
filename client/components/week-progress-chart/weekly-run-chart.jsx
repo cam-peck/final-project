98d5b1a7c3341c7a2c6404e2500b25d06feffa57
Cameron <cameronpeck317@gmail.com>
2023-03-01 19:48:18 +0000

    refactor weeklyrunchart to hooks

--- client/components/week-progress-chart/weekly-run-chart.jsx
+++ client/components/week-progress-chart/weekly-run-chart.jsx
@@ -3,26 +3,17 @@ import WeeklyRunChartHeader from './weekly-run-chart-header';
 import WeekChart from './week-chart';
 import { getWeeklyLineData } from '../../lib';
 
-export default class WeeklyRunChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hoveredIndex: null
-    };
-  }
-
-  render() {
-    const { data } = this.props;
-    const weeklyLineData = getWeeklyLineData(data);
-    return (
-      <div className="bg-white pl-6 pr-6 pt-5 pb-5 rounded-xl border border-gray-300 shadow-sm">
-        <section className="mb-6">
-          <WeeklyRunChartHeader data={weeklyLineData.weeklySumData} />
-        </section>
-        <section>
-          <WeekChart data={weeklyLineData} />
-        </section>
-      </div>
-    );
-  }
+export default function WeeklyRunChart(props) {
+  const { data } = props;
+  const weeklyLineData = getWeeklyLineData(data);
+  return (
+    <div className="bg-white pl-6 pr-6 pt-5 pb-5 rounded-xl border border-gray-300 shadow-sm">
+      <section className="mb-6">
+        <WeeklyRunChartHeader data={weeklyLineData.weeklySumData} />
+      </section>
+      <section>
+        <WeekChart data={weeklyLineData} />
+      </section>
+    </div>
+  );
 }
