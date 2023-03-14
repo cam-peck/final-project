db4f027b1c492dfccc0a7bc0d354dbcc5ab2b5b3
Cameron <cameronpeck317@gmail.com>
2023-03-14 17:26:27 +0000

    ensure that when users are already signed in they are redirected to the activities home page route instead of sign-in or register

--- client/pages/login.jsx
+++ client/pages/login.jsx
@@ -1,10 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import AuthForm from '../components/forms/auth-form';
 import { AppContext } from '../lib';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage(props) {
 
-  const { handleSignIn } = useContext(AppContext);
+  const { handleSignIn, user } = useContext(AppContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) navigate('/home/activities');
+  }, [user, navigate]);
 
   return (
     <div className="w-11/12 md:w-8/12 max-w-lg mx-auto bg-white pb-10 rounded-xl border border-gray-200 shadow-lg mt-8 mb-8 sm:mt-14">
