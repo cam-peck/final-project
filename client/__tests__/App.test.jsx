2a9690894d568290b237f1a077ab0331fabdcc97
Cameron Peck <cameronpeck317@gmail.com>
2023-03-16 21:47:21 +0000

    cleanup app.test file

--- client/__tests__/App.test.jsx
+++ client/__tests__/App.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../app';
 import { BrowserRouter } from 'react-router-dom';
@@ -7,16 +7,23 @@ import { BrowserRouter } from 'react-router-dom';
 const { getAllByRole } = screen;
 
 describe('Tests for landing page', () => {
-  render(
-    <App />, { wrapper: BrowserRouter }
-  );
+
+  beforeEach(() => {
+    render(
+      <App />, { wrapper: BrowserRouter }
+    );
+  });
 
   test('renders the sign-in page when no user is signed in', async () => {
     expect(getAllByRole('heading')).toHaveLength(2);
     expect(getAllByRole('button')).toHaveLength(3);
   });
 
-  // test('renders the home page when a user is signed in', async () => {
+  test('renders the sign-up page when register is clicked', async () => {
 
-  // });
+    const registerButton = screen.getByText('Register');
+    fireEvent.click(registerButton);
+    expect(screen.getAllByRole('button')).toHaveLength(1);
+  });
+
 });
