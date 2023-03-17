f4f6c0b8f6d51721959d13b236a0f3f1aea88fe8
Cameron Peck <cameronpeck317@gmail.com>
2023-03-17 17:20:20 +0000

    cleanup code across gpx runs files

--- client/components/cards/upload-run-card.jsx
+++ client/components/cards/upload-run-card.jsx
@@ -3,7 +3,7 @@ import Map from '../gmaps/map';
 import NoGpxFound from '../gmaps/no-gpx-found';
 
 export default function UploadRunCard(props) {
-  const { fileInputRef, toggleGpxTrue, handleGpxData, gpxPath, hasGpx } = props;
+  const { fileInputRef, setHasGpx, handleGpxData, gpxPath, hasGpx } = props;
   return (
     <section className="flex flex-col mb-4 pt-1">
       <div className="">
@@ -16,7 +16,7 @@ export default function UploadRunCard(props) {
       <div className="bg-blue-200 flex justify-between items-center rounded-br-xl rounded-bl-xl px-5 py-3">
         <p className="font-lora text-lg">Have GPS data?</p>
         <label htmlFor="file-input" className="text-center bg-blue-600 text-white px-7 py-3 rounded-3xl hover:cursor-pointer active:transform active:translate-y-[1px] select-none">Upload data</label>
-        <input id="file-input" type="file" name="file" accept=".gpx" onChange={event => { toggleGpxTrue(); handleGpxData(event); }} className="hidden" ref={fileInputRef}/>
+        <input id="file-input" type="file" name="file" accept=".gpx" onChange={event => { setHasGpx(true); handleGpxData(event); }} className="hidden" ref={fileInputRef}/>
       </div>
     </section>
   );
